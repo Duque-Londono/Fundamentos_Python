1452d00ad0b923e8d7306fdb62413248cb728280
Juan Duque <jd1020157@gmail.com>
2026-07-23 10:41:59 -0500

    Seccion 3 Terminada

---
+++ seccion3-Operadores/Operadores.py
@@ -0,0 +1,42 @@
+print(2+2)
+
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+print(6 // 4)
+print(6. // 4)
+
+print(-6 // 4)
+print(6. // -4)
+
+print(14 % 4)
+
+print(14 % -4)
+print(-14 % 4)
+print(-14 % -4)
+
+print(1 + 2 * 3 - 4 / 5 ** 6)
+print(1 + 2 * 3 - 4 / 5 ** 6)
+
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
