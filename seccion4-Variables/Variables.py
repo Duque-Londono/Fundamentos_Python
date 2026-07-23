ec53ee45edcc169b8254d44e0e1661258bbb6c53
Juan Duque <jd1020157@gmail.com>
2026-07-23 10:58:47 -0500

    Seccion 4 terminada

---
+++ seccion4-Variables/Variables.py
@@ -0,0 +1,34 @@
+var = 1
+print(var)
+
+var = 1
+account_balance = 1000.0
+client_name = 'Juan Duque'
+print(var, account_balance, client_name)
+print(var)
+
+var = "3.14.5"
+print("Python version: " + var)
+
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+var = 100
+var = 200 + 300
+print(var)
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = 100
+kilometers_to_miles = 100
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
