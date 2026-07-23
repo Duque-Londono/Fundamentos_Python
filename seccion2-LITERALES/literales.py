c9ee54f8a5ef656dbac9793f15b162c2153d649f
Juan Duque <jd1020157@gmail.com>
2026-07-23 10:23:25 -0500

    Seccion 2 terminada

---
+++ seccion2-LITERALES/literales.py
@@ -0,0 +1,31 @@
+print("2")
+print(2)
+
+print(0o123)
+print(0x123)
+
+num1 = 4.0
+num2 = 4
+
+print(num1, "Esto es un numero flotante")
+print(num2, "Esto es un numero entero")
+
+print(0.0000000000000000000001)
+
+print('I\'m Monty Python.')
+
+print(True > False)
+print(True < False)
+
+print(True == False)
+print(True != False)
+
+print(True and False)
+print(True or False)
+print(not True)
+print(not False)
+
+print(True is True)
+print(True is False)
+
+print("\"Estoy\"\n\"\"aprendiendo\"\"\n\"\"\"Python\"\"\"")
