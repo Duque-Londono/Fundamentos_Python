b011e1f5dee9c550ef413bc847299721cf2efc30
Juan Duque <jd1020157@gmail.com>
2026-07-23 09:35:47 -0500

    Seccion 1 terminada usando la funcion print

---
+++ seccion1-HolaMundo/FormatoSalida.py
@@ -0,0 +1,7 @@
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
---
+++ seccion1-HolaMundo/printArgumentos.py
@@ -0,0 +1,13 @@
+print("La Witsi Witsi Araña subió a su telaraña.")
+print("Vino la lluvia y se la llevó.")
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+print("La Witsi Witsi Araña" , "subió" , "a su telaraña.")
+
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
